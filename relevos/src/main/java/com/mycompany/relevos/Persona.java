689a02120b1028a8e36b7513f0690242c014b506
Senriquegonzalez <Segakiss@hotmail.com>
2020-03-19 15:39:48 -0500

    ajuste a la clase  clase persona

--- relevos/src/main/java/com/mycompany/relevos/Persona.java
+++ relevos/src/main/java/com/mycompany/relevos/Persona.java
@@ -5,10 +5,58 @@
  */
 package com.mycompany.relevos;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author sergio
  */
-public class Persona {
-    
+public class Persona extends Equipo {
+
+    Equipo equipo;
+    int metrosinicio;
+    int metrosrecorrido;
+    int id;
+
+    public Persona(Equipo equipo, int metrosinicio, int id) {
+
+        this.equipo = equipo;
+        this.metrosinicio = metrosinicio;
+        this.id = id;
+    }
+
+    @Override
+    public synchronized void run() {
+//fe
+        synchronized (equipo) {
+            while (id != equipo.contador) {
+                try {
+                    equipo.wait();
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Persona.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+
+            for (int i = 0; metrosrecorrido < 33; i++) {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException ex) {
+                    Logger.getLogger(Persona.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                int n = (int) (Math.random() * 3 + 1);
+                if (metrosrecorrido + n < 33) {
+                    metrosrecorrido = metrosrecorrido + n;
+                } else {
+                    metrosrecorrido = 33;
+                }
+
+            }
+
+            equipo.contador++;
+            equipo.notifyAll();
+        }
+
+    }
+
 }
