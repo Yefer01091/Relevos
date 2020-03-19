5ee9fbbcb4c5586770c5d8bf75ed76460e25711c
Senriquegonzalez <Segakiss@hotmail.com>
2020-03-19 15:28:53 -0500

    Se crea la clase carretera  y la clase persona

---
+++ relevos/src/main/java/com/mycompany/relevos/Equipo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practica;
+
+/**
+ *
+ * @author spinter
+ */
+public class Equipo extends Thread  {
+ 
+ 
+ int contador=0;
+
+    public Equipo() {
+                
+       
+    }
+     
+}
